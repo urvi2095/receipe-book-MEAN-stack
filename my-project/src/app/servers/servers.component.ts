dbed9ea9a342874f2a4b742454e472a8653c38a7
urvi2095 <maru.u@husky.neu.edu>
2019-04-23 17:20:38 -0400

    Adding Mean stack project

---
+++ my-project/src/app/servers/servers.component.ts
@@ -0,0 +1,30 @@
+import { Component} from '@angular/core';
+
+@Component({
+  // selector: '[app-servers]',
+  selector : 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent{
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = '';
+
+  constructor(){
+    setTimeout(() => {
+      this.allowNewServer = true;
+    } , 2000);
+
+  }
+
+  onCreateServer(){
+    this.serverCreationStatus = 'The server was created!';
+  }
+
+  onUpdateServerName(event : Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  
+
+}
